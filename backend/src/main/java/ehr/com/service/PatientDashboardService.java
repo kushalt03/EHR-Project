9d7b8b3c75f2ce5d57447c1908604efe1c07624a
kushalt03 <kushalbtaparia@gmail.com>
2026-04-25 22:06:18 +0530

    Updated DB config for deployment

--- backend/src/main/java/ehr/com/service/PatientDashboardService.java
+++ backend/src/main/java/ehr/com/service/PatientDashboardService.java
@@ -1,9 +1,9 @@
 package ehr.com.service;
 
-import ehr.com.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.data.repository.query.Param;
+
+import ehr.com.repository.PatientRepository;
 
 @Service
 public class PatientDashboardService {
